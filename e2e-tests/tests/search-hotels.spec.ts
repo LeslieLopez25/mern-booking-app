b7784f826fe39d0859ed56303931896cf7ae7944
Leslie Lopez <leslie2515lopez@gmail.com>
2025-07-26 00:32:34 -0600

    Created e2e test for hotel detail page

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -27,3 +27,14 @@ test("Should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Guadalajara")).toBeVisible();
   await expect(page.getByText("Happy Resort")).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("Guadalajara");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Happy Resort").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
+});
