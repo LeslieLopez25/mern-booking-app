d37769c9d81bbef96055dce633a27428a946932a
Leslie Lopez <leslie2515lopez@gmail.com>
2025-08-15 00:12:38 -0600

    Fixed the css for footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <div className="bg-blue-800 py-10">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center gap-4 sm:gap-0 text-center sm:text-left">
         <span className="text-3xl text-white font-bold tracking-tight">
           MernHolidays.com
         </span>
-        <span className="text-white font-bold tracking-tight flex gap-4">
+        <span className="text-white font-bold tracking-tight flex flex-col sm:flex-row gap-2 sm:gap-4">
           <p className="cursor-pointer">Privacy Policy</p>
           <p className="cursor-pointer">Terms of Service</p>
         </span>
