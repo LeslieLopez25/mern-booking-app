d0bb7920faf7306c89386afdeca3a62c5cc4a4ec
Leslie Lopez <leslie2515lopez@gmail.com>
2025-08-16 01:15:28 -0600

    Updated add hotel page and added navigation

--- client/src/pages/AddHotel.tsx
+++ client/src/pages/AddHotel.tsx
@@ -2,13 +2,19 @@ import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 import * as apiClient from "../api-client";
+import { useNavigate } from "react-router-dom";
 
 const AddHotel = () => {
   const { showToast } = useAppContext();
+  const navigate = useNavigate();
 
   const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
     onSuccess: () => {
       showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+
+      setTimeout(() => {
+        navigate("/my-hotels");
+      }, 3000);
     },
     onError: () => {
       showToast({ message: "Error Saving Hotel", type: "ERROR" });
